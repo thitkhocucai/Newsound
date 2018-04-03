8537696730da6d722c6650d76dfab601db45488b
thitkhocucai <37363776+thitkhocucai@users.noreply.github.com>
2018-04-03 11:08:24 +0300

    Update screen.h

--- screen.h
+++ screen.h
@@ -1,6 +1,7 @@
 #define ESC 0x1B
 #define BAR "\u2590"
 #define UNICODE 
+typedef enum FGCOLORS {BlACK = 30, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE}
 //function prototype
 void clearScreen(void);
 void gotoxy(int , int);
