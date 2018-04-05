2bb5a7a03feee80a7125399ddd3f58a5b807b8c8
thitkhocucai <37363776+thitkhocucai@users.noreply.github.com>
2018-04-05 15:32:00 +0300

    Delete screen.h

--- screen.h
+++
@@ -1,9 +0,0 @@
-#define ESC 0x1B
-#define BAR "\u2590"
-#define UNICODE 
-typedef enum FGCOLORS {BlACK = 30, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE}
-//function prototype
-void clearScreen(void);
-void gotoxy(int , int);
-void bar(int, double);
-void setColor(int);
