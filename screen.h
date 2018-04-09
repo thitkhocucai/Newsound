6a992c92a93a83d46bc9529e5cb42e82983b973e
thitkhocucai <37363776+thitkhocucai@users.noreply.github.com>
2018-04-09 06:26:38 +0000

    Delete screen.h

--- screen.h
+++
@@ -1,9 +0,0 @@
-#define ESC 0x1B
-#define BAR "\u2590"
-#define UNICODE 	//conditional compiling
-//function prototypes
-void clearscreen(void
-);
-void gotoxy(int , int);
-void bar(int , double);
-void setColor(int);
