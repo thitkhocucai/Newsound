a0821c0616d2ed71edc347269d11532fb7e15875
thitkhocucai <37363776+thitkhocucai@users.noreply.github.com>
2018-04-30 14:30:09 +0300

    Update sound.h

--- sound.h
+++ sound.h
@@ -3,23 +3,23 @@
 //#define DEBUG
 
 //constants definition
-#define SAMPLERATE 16000
-#define ESC 0x1b
+#define SAMPLERATE 16000 // define the sample rate
+#define ESC 0x1b // define the esc
 //define the wav header
-typedef struct {
+typedef struct { 
 	char chunkID[4]; // "RIFF"
 	unsigned int chunkSize;  // 4 bytes
 	char format[4]; // "WAVE"
 	char subchunk1ID[4]; // "fmt "
-	unsigned int subchunk1Size;
-	unsigned short audioFormat;
-	unsigned short numChannels;
-	unsigned int sampleRate;
-	unsigned int byteRate;
-	unsigned short blockAlign;
-	unsigned short bitsPerSample;
-	char subChunk2ID[4]; // "data"
-	unsigned int subChunk2Size;
+	unsigned int subchunk1Size; // unsigned subchunk
+	unsigned short audioFormat; // audio format
+	unsigned short numChannels; // numchannel
+	unsigned int sampleRate; // samplerate
+	unsigned int byteRate; // byterate
+	unsigned short blockAlign; // block Alight
+	unsigned short bitsPerSample; // Bitspersample
+	char subChunk2ID[4]; // "data" // subChunk
+	unsigned int subChunk2Size; // chubchunk
 }WAVHeader;
 
 // function prototypes
